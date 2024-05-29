384c2dfce8219876b57d6fab1be26e42888a2b17
ArielAVega <141455765+ArielAVega@users.noreply.github.com>
2024-05-29 09:25:38 -0300

    Demo BOotstrap using cards

---
+++ src/main/java/com/example/demo/DemobootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobootstrapApplication.class, args);
+	}
+
+}
